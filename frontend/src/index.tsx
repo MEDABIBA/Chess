b1c41a035b771610d2fadb16727e4fda52161f43
Slava <s.l.a.v.a.2007007@gmail.com>
2026-05-08 12:57:58 +0200

    Add react-helmed metadata

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,25 +5,32 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { StoreInitializer } from './provider/context';
 import { ToastContainer } from 'react-toastify';
+import { Helmet } from 'react-helmet-async';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
-    <BrowserRouter>
-      <StoreInitializer />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        pauseOnFocusLoss
-        closeOnClick
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-      <App />
-    </BrowserRouter>,
+    <Helmet>
+      <title>Chess</title>
+      <meta property="og:title" content={`Play to chess with me!`} />
+      <meta property="og:description" content="Join my chess game!" />
+
+      <BrowserRouter>
+        <StoreInitializer />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          pauseOnFocusLoss
+          closeOnClick
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+        <App />
+      </BrowserRouter>
+    </Helmet>,
   );
 }

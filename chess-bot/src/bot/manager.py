b3a7a64c58c46dc8da1e8f2554e990f8e1567627
Slava <s.l.a.v.a.2007007@gmail.com>
2026-05-20 13:27:39 +0200

    Implement socket connection

--- chess-bot/src/bot/manager.py
+++ chess-bot/src/bot/manager.py
@@ -1,13 +1,25 @@
 from schemas.schemas import GameInfo, GameId
 from session import Session
 import asyncio
+import os
+import httpx
 
 class Manager:
     def __init__(self):
+        self.username: str = os.environ["BOT_USERNAME"]
+        self.password: str = os.environ["BOT_PASSWORD"]
+        self.accessToken: str = ""
         self.sessions: dict[int, asyncio.Task] = {}
+        self.http = httpx.AsyncClient()
+
+    async def get_access_token(self):
+        res = await self.http.post("/auth/login", json={"username": self.username, "password": self.password})
+        data = res.json()["accessToken"]
+        self.accessToken = data
     
-    def start_game(self, body: GameInfo):
-        session = Session(body)
+    async def start_game(self, body: GameInfo):
+        await self.get_access_token()
+        session = Session(body, self.accessToken)
         self.sessions[body.gameId] = asyncio.create_task(session.run())
 
     def stop_game(self, gameId: GameId):
--- chess-bot/src/bot/session.py
+++ chess-bot/src/bot/session.py
@@ -1,11 +1,21 @@
+import socketio
 from schemas.schemas import GameInfo
 
 class Session:
-    def __init__(self, body: GameInfo):
+    def __init__(self, body: GameInfo, accessToken: str):
+      self.sio = socketio.AsyncClient()
       self.gameId = body.gameId
       self.color = body.color
       self.level = body.level
+      self.accessToken = accessToken
 
+    def handleListeners(self):
+      async def on_state(data):
+          pass
+      self.sio.on("state", on_state)
+      
     async def run(self):
-    #   socket connection here
-      return
+      await self.sio.connect("http://backend:3030", auth={"token": self.accessToken})
+      self.handleListeners()
+      await self.sio.wait()
+      
